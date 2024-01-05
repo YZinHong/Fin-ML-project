132d0eb46c3dfc18bd79290adc87be8ee9b9ffc3
jinhong <ygzh900@gmail.com>
2024-01-05 16:50:30 +0900

    first

---
+++ tests/test_agent_ebest.py
@@ -0,0 +1,14 @@
+import unittest
+from stocklab.agent.ebest import EBest
+import inspect
+import time
+
+class TestEBest(unittest.TestCase):
+    def setUp(self):
+        self.ebest = EBest("DEMO")
+        self.ebest.login()
+
+    def tearDown(self):
+        self.ebest.logout()
+
+
